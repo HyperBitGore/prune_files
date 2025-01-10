733ec511fd96ab3af8dd1d78b7c9775da8d554e4
HyperBitGore <bruiserjoe01@gmail.com>
2025-01-10 04:53:37 -0600

    better pruning

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <ios>
 #include <iostream>
 #include <fstream>
@@ -15,6 +16,23 @@ void pruneFiles (unsigned char c, std::string dir_path) {
     }
 }
 
+bool containsWords (std::vector<std::string>& words, std::string s) {
+    for (auto& i : words) {
+        if (s.find(i) != std::string::npos) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void pruneFilesWords (std::vector<std::string>& words, std::string dir_path) {
+    for (const auto & entry : std::filesystem::directory_iterator(dir_path)) {
+        std::string s = entry.path().string();
+        if (!containsWords(words, s)) {
+            std::remove(s.c_str());
+        }
+    }
+}
 
 int main () {
 
@@ -35,27 +53,50 @@ int main () {
         file.close();
     }*/
 
+    std::string mode;
+    std::cout << "Prune mode:\n 1. Prune based on chars.txt (characters found will prune files)\n 2. Prune files not containing words in words.txt\n";
+    std::cin >> mode;
+
     std::string s;
     std::cout << "Input the dir path to prune!\n";
     std::cin >> s;
+    if (mode.compare("1") == 0) {
+        std::cout <<"opening file\n";
+        std::ifstream r_file("chars.txt", std::ios::binary);
+        if (!r_file) {
+            std::cout << "Failed to read file!\n";
+            return -1;
+        }
+            r_file.seekg(0, std::ios::end);
+        std::streamsize size = r_file.tellg();
+        r_file.seekg(0, std::ios::beg);
+        std::cout << "File size: " << size << "\n";
+        //read each character and process
+        std::vector<char> buffer(size);
+        r_file.read(buffer.data(), size);
+        r_file.close();
+        for (char i : buffer) {
+            unsigned char c = (unsigned char)i;
+            pruneFiles(c, s);
+        }
+    } else if (mode.compare("2") == 0) {
+        std::cout <<"opening file\n";
+        std::ifstream r_file("words.txt");
+        if (!r_file) {
+            std::cout << "Failed to read file!\n";
+            return -1;
+        }
+        std::vector<std::string> words;
+        std::string line;
+        std::cout << "Reading words\n";
+        while(getline(r_file, line)) {
+            words.push_back(line);
+            std::cout << line << "\n";
+        }
+        std::cout << "Pruning files\n";
+        r_file.close();
+        pruneFilesWords(words, s);
+    }
 
-    std::cout <<"opening files\n";
-    std::ifstream r_file("chars.txt", std::ios::binary);
-    if (!r_file) {
-        std::cout << "Failed to read file!\n";
-        return -1;
-    }
-    r_file.seekg(0, std::ios::end);
-    std::streamsize size = r_file.tellg();
-    r_file.seekg(0, std::ios::beg);
-    std::cout << "File size: " << size << "\n";
-    //read each character and process
-    std::vector<char> buffer(size);
-    r_file.read(buffer.data(), size);
-    r_file.close();
-    for (char i : buffer) {
-        unsigned char c = (unsigned char)i;
-        pruneFiles(c, s);
-    }
     return 0;
 }
